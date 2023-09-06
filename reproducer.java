912fd814e3ff077abb7357b81951077a6572ddb8
Galder Zamarreño <galder@zamarreno.com>
2023-09-06 11:42:27 +0200

    Add a standalone reproducer Java class

---
+++ reproducer.java
@@ -0,0 +1,7 @@
+class reproducer {
+    public static void main(String[] args) throws ClassNotFoundException
+    {
+        System.out.println(Class.forName("com.ibm.bamoe.ilmt.quarkus.dmoe.DMOESwidTagGen_Bean"));
+        System.out.println(Class.forName("com.ibm.bamoe.ilmt.quarkus.dmoe.DMOESwidTagGen"));
+    }
+}
